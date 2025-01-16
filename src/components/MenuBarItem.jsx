c3744e9607fe5489d249ca9471da6a5beb4c472e
hosain ali <hosainahmed534745@gmail.com>
2025-01-16 17:54:57 +0600

    make all the nav link

--- src/components/MenuBarItem.jsx
+++ src/components/MenuBarItem.jsx
@@ -1,103 +1,171 @@
-import React from 'react';
-import { Menu, Dropdown, Layout, Typography } from 'antd';
+import React from "react";
+import { Menu, Dropdown, Layout, Typography } from "antd";
 import {
-    AppstoreOutlined,
-    UnorderedListOutlined,
-    ProfileOutlined,
-    FileSearchOutlined,
-    ShoppingCartOutlined,
-} from '@ant-design/icons';
+  AppstoreOutlined,
+  UnorderedListOutlined,
+  ProfileOutlined,
+  FileSearchOutlined,
+  ShoppingCartOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 const { Header } = Layout;
 const { Text } = Typography;
 
 const categories = [
-    { key: '1', icon: <AppstoreOutlined />, label: 'Ghee & Oil' },
-    { key: '2', icon: <UnorderedListOutlined />, label: 'Original Organic Honey' },
-    { key: '3', icon: <ProfileOutlined />, label: 'Nuts & Dates' },
-    { key: '4', icon: <FileSearchOutlined />, label: 'Organic Spices' },
-    { key: '5', icon: <AppstoreOutlined />, label: 'Organic Oil' },
-    { key: '6', icon: <ShoppingCartOutlined />, label: 'Rice, Pulse & Grains' },
-    { key: '7', icon: <AppstoreOutlined />, label: 'Super Foods' },
-    { key: '8', icon: <UnorderedListOutlined />, label: 'Sweeteners & Dairy' },
-    { key: '9', icon: <ProfileOutlined />, label: 'Pickle & Chutney' },
-    { key: '10', icon: <FileSearchOutlined />, label: 'Dry Fruits & Dates' },
+  {
+    key: "1",
+    icon: <AppstoreOutlined />,
+    label: "Ghee & Oil",
+    path: "/categories/ghee-oil",
+  },
+  {
+    key: "2",
+    icon: <UnorderedListOutlined />,
+    label: "Original Organic Honey",
+    path: "/categories/honey",
+  },
+  {
+    key: "3",
+    icon: <ProfileOutlined />,
+    label: "Nuts & Dates",
+    path: "/categories/nuts-dates",
+  },
+  {
+    key: "4",
+    icon: <FileSearchOutlined />,
+    label: "Organic Spices",
+    path: "/categories/spices",
+  },
+  {
+    key: "5",
+    icon: <AppstoreOutlined />,
+    label: "Organic Oil",
+    path: "/categories/oil",
+  },
+  {
+    key: "6",
+    icon: <ShoppingCartOutlined />,
+    label: "Rice, Pulse & Grains",
+    path: "/categories/rice-grains",
+  },
+  {
+    key: "7",
+    icon: <AppstoreOutlined />,
+    label: "Super Foods",
+    path: "/categories/super-foods",
+  },
+  {
+    key: "8",
+    icon: <UnorderedListOutlined />,
+    label: "Sweeteners & Dairy",
+    path: "/categories/sweeteners-dairy",
+  },
+  {
+    key: "9",
+    icon: <ProfileOutlined />,
+    label: "Pickle & Chutney",
+    path: "/categories/pickle-chutney",
+  },
+  {
+    key: "10",
+    icon: <FileSearchOutlined />,
+    label: "Dry Fruits & Dates",
+    path: "/categories/dry-fruits-dates",
+  },
 ];
 
 const navbarItems = [
-    { key: 'home', label: 'Home' },
-    { key: 'products', label: 'All Products' },
-    { key: 'updates', label: 'Updates' },
-    { key: 'orderTrack', label: 'Order Track' },
-    { key: 'contact', label: 'Contact Us' },
+  { key: "home", label: "Home", path: "/" },
+  { key: "products", label: "All Products", path: "/products" },
+  { key: "updates", label: "Updates", path: "/updates" },
+  { key: "orderTrack", label: "Order Track", path: "/order-track" },
+  { key: "contact", label: "Contact Us", path: "/contact" },
 ];
 
 const categoriesMenu = (
-    <Menu
+  <Menu
+    style={{
+      backgroundColor: "#008000",
+      borderRadius: "0px",
+    }}
+  >
+    {categories.map((category) => (
+      <Menu.Item
+        key={category.key}
         style={{
-            backgroundColor: '#008000',
-            borderRadius: '0px',
+          backgroundColor: "#008000",
+          borderRadius: "0px",
         }}
-    >
-        {categories.map((category) => (
-            <Menu.Item
-                key={category.key}
-                icon={category.icon}
-                style={{
-                    backgroundColor: '#008000',
-                    color: 'white',
-                    borderRadius: '0px',
-                }}
-            >
-                {category.label}
-            </Menu.Item>
-        ))}
-    </Menu>
+      >
+        <Link
+          to={category.path}
+          style={{
+            color: "white",
+            textDecoration: "none",
+          }}
+        >
+          {category.icon} {category.label}
+        </Link>
+      </Menu.Item>
+    ))}
+  </Menu>
 );
 
 const MenuBarItem = () => {
-    return (
-        <Layout style={{ backgroundColor: '#008000', padding: '0 20px', marginTop: '20px' }}>
-            <Header className='area' style={{ backgroundColor: '#008000', padding: '0 20px' }}>
-                <Dropdown
-                    overlay={categoriesMenu}
-                    trigger={['hover']}
-                    overlayStyle={{
-                        backgroundColor: '#008000',
-                        borderRadius: '0px',
-                    }}
-                    placement="bottomLeft"
-                >
-                    <Text
-                        style={{
-                            color: 'white',
-                            fontSize: '16px',
-                            cursor: 'pointer',
-                            marginRight: '20px',
-                        }}
-                    >
-                        <AppstoreOutlined style={{ marginRight: '5px' }} />
-                        Categories
-                    </Text>
-                </Dropdown>
+  return (
+    <Layout
+      style={{
+        backgroundColor: "#008000",
+        padding: "0 20px",
+        marginTop: "20px",
+      }}
+    >
+      <Header
+        className="area"
+        style={{ backgroundColor: "#008000", padding: "0 20px" }}
+      >
+        <Dropdown
+          overlay={categoriesMenu}
+          trigger={["hover"]}
+          overlayStyle={{
+            backgroundColor: "#008000",
+            borderRadius: "0px",
+          }}
+          placement="bottomLeft"
+        >
+          <Text
+            style={{
+              color: "white",
+              fontSize: "16px",
+              cursor: "pointer",
+              marginRight: "20px",
+            }}
+          >
+            <AppstoreOutlined style={{ marginRight: "5px" }} />
+            Categories
+          </Text>
+        </Dropdown>
 
-                {/* Other Navbar Items */}
-                {navbarItems.map((item) => (
-                    <Text
-                        key={item.key}
-                        style={{
-                            color: 'white',
-                            fontSize: '16px',
-                            cursor: 'pointer',
-                            marginLeft: '20px',
-                        }}
-                    >
-                        {item.label}
-                    </Text>
-                ))}
-            </Header>
-        </Layout>
-    );
+        {/* Navbar Items with Links */}
+        {navbarItems.map((item) => (
+          <Link
+            key={item.key}
+            to={item.path}
+            style={{
+              color: "white",
+              fontSize: "16px",
+              cursor: "pointer",
+              marginLeft: "20px",
+              textDecoration: "none",
+            }}
+          >
+            {item.label}
+          </Link>
+        ))}
+      </Header>
+    </Layout>
+  );
 };
 
 export default MenuBarItem;
